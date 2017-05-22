9da96e73acb0c43a1fe64ff12326e4f587524115
deimss <deimss95@gmail.com>
2017-05-22 22:59:12 +0300

    Add files for homework1_JS

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = prompt("What is your name?","Bohdan");
+var time = prompt("When are you free today?", "17:00");
+var place = prompt("Where would you like to go in Lviv?","Cafe");
+
+alert(name + ' has a meeting today at '+time+ ' at ' + place + ' in Lviv');
+console.log(name + ' has a meeting today at '+time+ ' at ' + place + ' in Lviv');
